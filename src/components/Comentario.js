4f64e67efa81deba1398be12c692b76cfcbaa8d3
batista7557 <gabriel.bat2001@gmail.com>
2023-01-02 23:36:06 -0300

    Iniciando e construindo componente

---
+++ src/components/Comentario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comentario.css'
+
+//JSX
+const Comentario = () => (
+    <div className="Comentario">
+        <div>João:</div>
+        <div>Olá, eu sou um componente</div>
+    </div>
+)
+
+export default Comentario;
